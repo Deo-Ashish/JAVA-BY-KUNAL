49b7629cfe6310fc4ef7a89625a6cb00f342353c
Deo-Ashish <deoashish942@gmail.com>
2026-08-01 23:01:50 +0530

    patterns3

--- Rough/src/Patterns.java
+++ Rough/src/Patterns.java
@@ -273,13 +273,31 @@
 
 public class Patterns {
     static void main(String[] args) {
-        pattern1(5);
+        pattern3(5);
 
     }
 
     static void pattern1(int n) {
         for (int row = 0; row < n; row++) {
             for (int col = 0; col < n; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern2(int n) {
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < row; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern3(int n) {
+        for (int row = n; row > 0; row--) {
+            for (int col = 0; col < row; col++) {
                 System.out.print("*");
             }
             System.out.println();
