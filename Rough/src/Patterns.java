ccf0b3d272223dd5a0892d471c5d052d3c90c7c7
Deo-Ashish <deoashish942@gmail.com>
2026-08-01 22:58:00 +0530

    patterns1

--- Rough/src/Patterns.java
+++ Rough/src/Patterns.java
@@ -273,8 +273,17 @@
 
 public class Patterns {
     static void main(String[] args) {
-        pattern5(5);
+        pattern1(5);
 
+    }
+
+    static void pattern1(int n) {
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
     static void pattern5(int n) {
