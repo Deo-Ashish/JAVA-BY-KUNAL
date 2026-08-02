dde85eaf4170f832c43605469e766db53eccc4cc
Deo-Ashish <deoashish942@gmail.com>
2026-08-02 13:11:44 +0530

    pattern 15

--- Rough/src/Patterns.java
+++ Rough/src/Patterns.java
@@ -477,7 +477,60 @@ public class Patterns {
         }
     }
 
+
+    static void pattern15(int n) {
+      //   15.   *
+      //        * *
+      //       *   *
+      //      *     *
+      //     *       *
+      //      *     *
+      //       *   *
+      //        * *
+      //         *
+
+        for (int row = 0; row < n; row++) {
+
+            // Spaces
+            for (int space = 0; space < n - row - 1; space++) {
+                System.out.print(" ");
+            }
+
+            // Stars
+            for (int col = 0; col < 2 * row + 1; col++) {
+
+                if (col == 0 || col == 2 * row) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+
+        for (int row = n - 2; row >= 0; row--) {
+
+            // Spaces
+            for (int space = 0; space < n - row - 1; space++) {
+                System.out.print(" ");
+            }
+
+            // Stars
+            for (int col = 0; col < 2 * row + 1; col++) {
+
+                if (col == 0 || col == 2 * row) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+
     static void main(String[] args) {
-        pattern14(5);
+        pattern15(5);
     }
 }
