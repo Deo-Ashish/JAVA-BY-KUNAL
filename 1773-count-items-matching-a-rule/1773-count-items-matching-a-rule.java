1926fdcd1c0975f544963d16ff929e837ea1b5b0
Deo Ashish Kumar Gupta <deoashish942@gmail.com>
2026-08-13 07:34:30 +0530

    Time: 4 ms (96.53%), Space: 49.7 MB (49.29%) - LeetHub

---
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+
+        int count = 0;
+        int index = 0;
+
+        if (ruleKey.equals("type")) {
+            index = 0;
+        } else if (ruleKey.equals("color")) {
+            index = 1;
+        } else {
+            index = 2;
+        }
+
+        for (List<String> row : items) {
+            if (row.get(index).equals(ruleValue)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
