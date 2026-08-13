e9b7b923b87dbc9e0b94b914c9a419b127e1c8e7
Deo Ashish Kumar Gupta <deoashish942@gmail.com>
2026-08-13 06:45:22 +0530

    Time: 1 ms (82.26%), Space: 43 MB (33.94%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        boolean[] freq = new boolean[26];
+        boolean[] seen = new boolean[26];
 
         for (char ch : sentence.toCharArray()) {
-            freq[ch - 'a'] = true;
+            seen[ch - 'a'] = true;
         }
 
-        for (char ch = 'a'; ch <= 'z'; ch++) {
-            if (!freq[ch - 'a']) {
+        for (boolean present : seen) {
+            if (!present)
                 return false;
-            }
         }
+
         return true;
     }
 }
