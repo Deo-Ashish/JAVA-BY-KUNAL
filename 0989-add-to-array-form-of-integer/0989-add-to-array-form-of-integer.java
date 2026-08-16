a72af027ab3c8af12daa0b147b001b171266cd1d
Deo Ashish Kumar Gupta <deoashish942@gmail.com>
2026-08-16 13:31:47 +0530

    Time: 55 ms (22.82%), Space: 47.9 MB (33.86%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
@@ -1,56 +1,33 @@
 class Solution {
     public List<Integer> addToArrayForm(int[] num, int k) {
 
+        List<Integer> res = new ArrayList<>();
+
         int e = num.length - 1;
         int carry = 0;
 
-        while (k > 0 || carry > 0) {
+        while (e >= 0 || k > 0 || carry > 0) {
 
-            int l = k % 10;
-            k = k / 10;
+            int sum = carry;
 
             if (e >= 0) {
-                num[e] += l + carry;
-            } else {
-                // k has more digits than num
-                ArrayList<Integer> res = new ArrayList<>();
-
-                int sum = l + carry;
-                res.add(0, sum % 10);
-                carry = sum / 10;
-
-                while (k > 0) {
-                    sum = k % 10 + carry;
-                    res.add(0, sum % 10);
-                    carry = sum / 10;
-                    k = k / 10;
-                }
-
-                for (int i = 0; i < num.length; i++) {
-                    res.add(num[i]);
-                }
-
-                if (carry > 0) {
-                    res.add(0, carry);
-                }
-
-                return res;
+                sum += num[e];
+                e--;
             }
 
-            carry = num[e] / 10;
-            num[e] = num[e] % 10;
+            if (k > 0) {
+                sum += k % 10;
+                k /= 10;
+            }
 
+            res.add(0, sum % 10);
+            carry = sum / 10;
+        }
+
+        // If there are remaining digits in num
+        while (e >= 0) {
+            res.add(0, num[e]);
             e--;
-        }
-
-        ArrayList<Integer> res = new ArrayList<>();
-
-        if (carry > 0) {
-            res.add(carry);
-        }
-
-        for (int i = 0; i < num.length; i++) {
-            res.add(num[i]);
         }
 
         return res;
