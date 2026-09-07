fbc204a3cd9bbdf4cb5e2e8eb332aef5befff2e6
Deo Ashish Kumar Gupta <deoashish942@gmail.com>
2026-09-07 22:00:37 +0530

    Time: 0 ms (100%), Space: 44.9 MB (12.36%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -2,7 +2,6 @@ class Solution {
     public int searchInsert(int[] nums, int target) {
         int low = 0;
         int high = nums.length - 1;
-        int last = 0;
 
         while (low <= high) {
             int mid = low + (high - low) / 2;
@@ -11,25 +10,15 @@ class Solution {
                 return mid;
             }
 
-            else if (nums[high] < target) {
-                return high + 1;
-            }
-
-            // else if (nums[low] > target) {
-            //     return low;
-            // }
-
             else if (nums[mid] < target) {
                 low = mid + 1;
-                last = mid;
             }
 
             else {
                 high = mid - 1;
-                last = mid;
             }
         }
 
-        return last;
+        return low;
     }
 }
