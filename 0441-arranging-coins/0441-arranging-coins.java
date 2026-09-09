bc327508d6be56d9ff276bd0a1758b93cf255914
Deo Ashish Kumar Gupta <deoashish942@gmail.com>
2026-09-09 21:39:31 +0530

    Time: 6 ms (30.72%), Space: 41.8 MB (99.81%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,15 +1,13 @@
 class Solution {
     public int arrangeCoins(int n) {
         int i = 1;
-        int count = 1;
-
+        int count = 0;
 
         while (n >= 0) {
-
             n = n - i;
             i++;
             count++;
         }
-        return count - 2;
+        return count - 1;
     }
 }
