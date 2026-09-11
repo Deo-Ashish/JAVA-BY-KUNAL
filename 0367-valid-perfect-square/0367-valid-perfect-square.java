7a1a69aaed0d7431d7230cba90d80733c2f0530d
Deo Ashish Kumar Gupta <deoashish942@gmail.com>
2026-09-11 22:44:27 +0530

    Time: 0 ms (100%), Space: 41.9 MB (74.23%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,11 +1,23 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-        boolean ans = false;
-        for (long i = 1; i * i <= num; i++) {
-            if (i * i == num) {
+
+        long low = 1;
+        long high = num;
+
+        while (low <= high) {
+            long mid = low + (high - low) / 2;
+
+            if (mid * mid == num) {
                 return true;
+            } 
+            else if (mid * mid < num) {
+                low = mid + 1;
+            } 
+            else {
+                high = mid - 1;
             }
         }
+
         return false;
     }
 }
