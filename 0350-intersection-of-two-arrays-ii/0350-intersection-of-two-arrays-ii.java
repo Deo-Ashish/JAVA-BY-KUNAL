53ff2319ae80fc7b67fc4e33c24ecdfc34ce2e0a
Deo Ashish Kumar Gupta <deoashish942@gmail.com>
2026-09-14 19:22:52 +0530

    Time: 0 ms (100%), Space: 44.8 MB (77.14%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,21 +1,25 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        int[] ans = new int[Math.min(nums1.length, nums2.length)];
+
+        int[] freq = new int[1001];
+        int[] result = new int[Math.min(nums1.length, nums2.length)];
+
+        // Store frequency of nums1
+        for (int num : nums1) {
+            freq[num]++;
+        }
+
         int count = 0;
 
-        for (int i = 0; i < nums1.length; i++) {
-            for (int j = 0; j < nums2.length; j++) {
-
-                if (nums1[i] == nums2[j]) {
-                    ans[count] = nums1[i];
-                    count++;
-
-                    nums2[j] = -1; // mark as used
-                    break;
-                }
+        // Check nums2
+        for (int num : nums2) {
+            if (freq[num] > 0) {
+                result[count] = num;
+                count++;
+                freq[num]--;
             }
         }
 
-        return java.util.Arrays.copyOf(ans, count);
+        return java.util.Arrays.copyOf(result, count);
     }
 }
